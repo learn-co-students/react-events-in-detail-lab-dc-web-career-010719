0cf3c26710dec20e87f1c53a6487926bb4960da7
Dusty211 <kylebenz@hotmail.com>
2019-03-13 16:44:20 -0400

    cowabunga

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
-// Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+export default class CoordinatesButton extends Component {
+
+  getCoords = (e) => {
+    const array = [e.clientX, e.clientY]
+    this.props.onReceiveCoordinates(array)
+  }
+
+  render() {
+    return(
+      <button onClick={this.getCoords} />
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
-// Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+export default class DelayedButton extends Component {
+
+  clickFnc = (e) => {
+    e.persist()
+    setTimeout(() => this.props.onDelayedClick(e), this.props.delay)
+  }
+
+  render() {
+    return(
+      <button onClick={this.clickFnc} />
+    )
+  }
+}
